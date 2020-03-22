c3837f8962a8f3c5653bc4d33cfcd4ed8938be37
Dying-fish <2011710343@qq.com>
2020-03-22 23:19:11 +0800

    infomation

--- src/JavaScript/linkage.js
+++ src/JavaScript/linkage.js
@@ -1,3 +1,4 @@
+/*定义各个国家的城市选项*/
 var area = [
     ["重庆", "四川", "上海","哈尔滨"],
     ["东京", "大阪","北海道","名古屋"],
@@ -7,9 +8,11 @@ var area = [
 function changeSelect() {
     var selectCoutry = document.form1.country;
     var selectCity = document.form1.city;
+    /*获取选择的国家对应的城市数组*/
     var countryArea = area[selectCoutry.selectedIndex - 1];
     selectCity.length = 1;
     for(var i=0; i < countryArea.length; i++){
+      /*创建城市选项*/
       selectCity[i+1] = new Option(countryArea[i],countryArea[i]);
     }
 }
--- src/JavaScript/preview.js
+++ src/JavaScript/preview.js
@@ -1,8 +1,10 @@
+/*定义显示预览图的函数*/
 function showFile() {
   var preview = document.querySelector('img');
   var file    = document.querySelector('input[type=file]').files[0];
+  /*读取上传文件*/
   var reader  = new FileReader();
-
+  /*将网页种空图片的src改成上传文件*/
   reader.onloadend = function () {
     preview.src = reader.result;
   }
